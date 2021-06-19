b022af71cb8d2b6b9e84cdc509b964c4afc51827
fahraday <ghosterinc@gmail.com>
2021-06-18 20:29:06 -0400

    adding basic styles for the sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/NavBarr/Nav';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './app.css';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
     <Router>
-    <Navbar></Navbar>
+      <SideBar />
+      <Navbar />
     </Router>
   );
 }
